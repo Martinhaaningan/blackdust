52a141820daf4b15b58110a1b3c170270e8dff5a
mrBanden <martinhaaningan@gmail.com>
2021-11-22 09:58:17 +0100

    kvadratisk rendering

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -43,12 +43,15 @@ var Game = {};
 
 Game.setBoard = function () {
     this.board = $('board');
+    this.svg = $('svg');
     let body = $("body");
     let width = body.clientWidth;  
     this.board.setAttribute("width", width);
+    this.svg.setAttribute("width", width);
     let realWidth = this.board.clientWidth;
-    let height = (realWidth / 19) * 10;
+    let height = 770;
     this.board.setAttribute("height", height + '%');
+    this.svg.setAttribute("height", height + '%');
     this.board.style.backgroundColor ="black";
 };
 
@@ -58,7 +61,7 @@ Game.load = function () {
     ];
 };
 
-
+ 
 Game.run = function (context) {
   this.ctx = context;
     var p = this.load();
@@ -69,45 +72,68 @@ Game.run = function (context) {
 }
 
 var map = {
-    cols: 9,
-    rows: 9,
+    cols: 5,
+    rows: 5,
     twidth: 160, // Tile height og width i pixels
     theight: 140,
+    tx: 120,
+    ty: 140,
     tiles: [
-        1, 3, 3, 3, 1, 1, 3, 1, 2,
-        1, 1, 1, 1, 1, 1, 1, 1, 5,
-        1, 1, 1, 1, 1, 2, 1, 1, 2,
-        1, 1, 1, 1, 1, 1, 1, 1, 2,
-        1, 1, 1, 2, 4, 1, 1, 1, 5,
-        1, 1, 1, 1, 2, 1, 1, 1, 1,
-        1, 1, 1, 1, 2, 1, 1, 1, 5,
-        1, 1, 1, 1, 2, 1, 1, 1, 2,
-        1, 2, 5, 2, 1, 1, 2, 1, 5
+        0, 0, 0, 0, 0,  
+        0, 1, 2, 2, 0,  
+        0, 3, 4, 1, 0,
+        0, 0, 5, 0, 0,
+        0, 0, 0, 0, 0
     ],
     getTile: function (col, row) {
         return this.tiles[row * map.cols + col];
     }
 };
 
-Game.render = function () {
+function tileClickable (x, y, width, height, c, r) {
+  let tile = document.createElementNS("http://www.w3.org/2000/svg", 'polygon');
+  tile.setAttribute('width', width);
+  tile.setAttribute('height', height);
+  tile.setAttribute('points', "40,1 120,1 159,69 119,139 41,139 1,69");
+  tile.setAttribute("transform", "translate("+ x +","+ y +")");
+  tile.setAttribute("stroke-width", "2px");
+  tile.setAttribute("stroke", "#1C336A");
+  tile.setAttribute("fill","transparent");
+  tile.setAttribute("id", c+","+r);
+  tile.setAttribute("class", "tile");
+  let svg = $('svg');
+  svg.appendChild(tile);
+}
 
+Game.render = function () {
     for (var c = 0; c < map.cols; c++) {
         for (var r = 0; r < map.rows; r++) {
-
+            
             var tile = map.getTile(c, r);
+            let tx = map.tx*c;
+            let ty = map.ty*r;
+            if (c % 2 == 1) {
+                ty = ty + 70;
+              }
+
+            if (tile == 0) {
+              tileClickable(tx,ty, map.twidth, map.theight, c, r);
+            }
+
             if (tile !== 0) { // 0 => empty tile
+
                 this.ctx.drawImage(
                     this.tileAtlas, // image
                     (tile - 1) * map.twidth, // source x
                     0, // source y
                     map.twidth, // source width
                     map.theight, // source height
-                    
-                    c * map.twidth,  // target x
-                    r * map.theight, // target y
+                    tx,  // target x
+                    ty, // target y
                     map.twidth, // target width
                     map.theight // target height
                 );
+                tileClickable(tx,ty, map.twidth, map.theight, c, r);
             }
         }
     }
